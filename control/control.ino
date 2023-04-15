b8dfd23c9b8c57f5bb320e2e0409187a7fadcebf
Alex Lee <alee428@calpoly.edu>
2023-04-15 00:12:18 -0700

    implemented recordSounding(), added documentation on USF data format

--- control/control.ino
+++ control/control.ino
@@ -8,48 +8,70 @@
 // Define data arrays
 #define NUM_SAMPLES 1000
 
+
+#define SAMPLE_TIME_INTERVAL_MICROSEC 100 // Delay between each sample
+
+struct sounding {
+  unsigned long time;
+  int voltage;
+};
+
+sounding* recordSounding(unsigned long mu_s) {
+  unsigned long t1 = micros();
+  unsigned long t2 = micros() + mu_s;
+
+  int numSamples = (int)(mu_s / SAMPLE_TIME_INTERVAL_MICROSEC) + 10;
+
+  sounding s[numSamples];
+
+  int i = 0;
+  
+  while (micros() < t2) {
+    delayMicroseconds(SAMPLE_TIME_INTERVAL_MICROSEC);
+
+    s[i].time = micros() - t1;
+    s[i].voltage = analogRead(RECEIVER_PIN);
+    
+    i++;
+  }
+
+  return s;
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
   // Define pin numbers for transmitter and receiver
+
+  /*
   double times[NUM_SAMPLES];
   double voltages[NUM_SAMPLES];
 
   // Conduct TEM survey
   for (int i = 0; i < NUM_SAMPLES; i++) {
-	// Turn on transmitter
+    // Turn on transmitter
     digitalWrite(TRANSMITTER_PIN, HIGH);
 
     // Wait for delay time
     delayMicroseconds(DELAY_TIME_US);
 
-	// Read receiver voltage
-	voltages[i] = analogRead(RECEIVER_PIN);
+    // Read receiver voltage
+    voltages[i] = analogRead(RECEIVER_PIN);
 
-	// Record time
-	times[i] = (double)i * SAMPLE_TIME_US;
+    // Record time
+    times[i] = (double)i * SAMPLE_TIME_US;
 
-	// Turn off transmitter
-	digitalWrite(TRANSMITTER_PIN, LOW);
+    // Turn off transmitter
+    digitalWrite(TRANSMITTER_PIN, LOW);
 
-	// Wait for sample time
-	delayMicroseconds(SAMPLE_TIME_US);
+    // Wait for sample time
+    delayMicroseconds(SAMPLE_TIME_US);
   }
+  */
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
-
-void recordVoltage(long int ms) {
-  long int t1 = millis();
-  long int t2 = millis() + ms;
-
-  double t[ms + 1];
-  double v[ms + 1];
   
-  while (millis() > t2) {
-	delayMicroseconds(1000 * SAMPLE_TIME_INTERVAL);
-	
-  }
 }
